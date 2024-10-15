eda3a2d5a2d54ed1d9cdb205336cbca027161475
Grzegorzc1 <s207318@sggw.edu.pl>
2024-10-15 11:03:36 +0200

    Add files via upload
    Rozwiązanie zadania1

---
+++ 2-IntroToR2/zadanie1.R
@@ -0,0 +1,17 @@
+
+  
+  4.  **W skrypcie napisz komentarze** opisujące funkcje poszczególnych paneli.
+  #--------funkcje poszczególnychpaneli----------
+# Source (Edytor skryptów)służy do pisania skryptów, a funkcje nie są automatycznie wykonywane
+  #Console(Konsola) służy do wykożystywania skryptów
+  #Enviorment (Środowisko) zapisywane są w nim funkcje i wartości przypisane
+  #Panel wyjściowy służy do dostępu dosciezek ipakietów
+  
+
+5.  **Znajdź w RStudio ustawienia** i **zmień motyw kolorystyczny** na taki, który najbardziej Ci odpowiada.
+
+6.  **Napisz w komentarzu**, jaki motyw wybrałeś/aś i dlaczego.
+# wybrałem  motyw cobalt ponieważ pasował mi kolorystycznie
+7.  **Zapisz skrypt** jako `zadanie1.R` w swoim folderze.
+
+8.  **Prześlij plik do swojego repozytorium na GitHub**.
